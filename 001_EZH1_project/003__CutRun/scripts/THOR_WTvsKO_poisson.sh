fd00e357d9d3308ce9adcf0636627e556d8a3f0b
Thomas Roule <110408206+RouleThomas@users.noreply.github.com>
2023-06-16 17:02:24 -0400

    bigwig merge median THOR cut run

---
+++ 001_EZH1_project/003__CutRun/scripts/THOR_WTvsKO_poisson.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --mem=250G
+#SBATCH --time=200:00:00
+
+
+rgt-THOR --name WTvsKO --merge --output-dir output/THOR/THOR_WTvsKO_poisson --poisson --deadzones ../../Master/meta/hg38-blacklist.v2.bed --pvalue 0.1 --scaling-factors 1.412253844,1.186123259,1.337020479,1.40842928,1.502057669,1.487749425,0.449980991,1.883122496  output/THOR/WTvsKO.config
+
+
---
+++ 001_EZH1_project/003__CutRun/scripts/bigwigmerge_THOR_WTvsHET_poisson.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --mem=400G
+#SBATCH --time=100:00:00
+
+
+# Merge bigwig and compute median into a bedgraph
+wiggletools write_bg output/THOR/THOR_WTvsHET_poisson/WTvsHET-s1_median.bedGraph \
+    median output/THOR/THOR_WTvsHET_poisson/WTvsHET-s1-rep0.bw \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s1-rep1.bw \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s1-rep2.bw \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s1-rep3.bw
+wiggletools write_bg output/THOR/THOR_WTvsHET_poisson/WTvsHET-s2_median.bedGraph \
+    median output/THOR/THOR_WTvsHET_poisson/WTvsHET-s2-rep0.bw \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s2-rep1.bw \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s2-rep2.bw \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s2-rep3.bw  
+
+
+# Sort the bedgraph 
+bedtools sort -i output/THOR/THOR_WTvsHET_poisson/WTvsHET-s1_median.bedGraph > \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s1_median.sorted.bedGraph
+bedtools sort -i output/THOR/THOR_WTvsHET_poisson/WTvsHET-s2_median.bedGraph > \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s2_median.sorted.bedGraph
+
+
+    
+# Convert bedgraph to bigwig
+bedGraphToBigWig output/THOR/THOR_WTvsHET_poisson/WTvsHET-s1_median.sorted.bedGraph \
+    ../../Master/meta/GRCh38_chrom_sizes.tab \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s1_median.bw
+bedGraphToBigWig output/THOR/THOR_WTvsHET_poisson/WTvsHET-s2_median.sorted.bedGraph \
+    ../../Master/meta/GRCh38_chrom_sizes.tab \
+    output/THOR/THOR_WTvsHET_poisson/WTvsHET-s2_median.bw
