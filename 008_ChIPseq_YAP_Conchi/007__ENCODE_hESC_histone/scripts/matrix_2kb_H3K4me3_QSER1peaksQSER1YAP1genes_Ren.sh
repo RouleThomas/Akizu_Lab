a7ce74d0a1559bc68addef9087e082079a846055
Thomas Roule <110408206+RouleThomas@users.noreply.github.com>
2025-06-06 02:29:15 +0200

    up

---
+++ 008_ChIPseq_YAP_Conchi/007__ENCODE_hESC_histone/scripts/matrix_2kb_H3K4me3_QSER1peaksQSER1YAP1genes_Ren.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH --mem=200G
+#SBATCH --time=200:00:00
+#SBATCH --cpus-per-task=6
+
+
+computeMatrix reference-point --referencePoint center \
+    -b 2000 -a 2000 \
+    -R ../001__ChIPseq_V1/output/homer/QSER1_YAP1_1192genes_ChIPseeker_noHeader.bed \
+    -S ../001__ChIPseq_V1/output/THOR/THOR_hESC_QSER1_WTvsYAPKO/hESCQSER1WTvsYAPKO-s1_median.bw ../003__ChIPseq_pluripotency/output/bigwig/hESC_WT_YAP1_R1.unique.dupmark.sorted.bw output/ENCODE/Ren_H3K4me1.bigWig output/ENCODE/Ren_H3K27ac.bigWig output/ENCODE/Ren_H3K36me3.bigWig output/ENCODE/Ren_H3K27me3.bigWig ../001__ChIPseq_V1/output/THOR/THOR_hESC_EZH2_WTvsYAPKO/hESCEZH2WTvsYAPKO-s1_median.bw ../008__ChIPseq_Nipbl/output/bigwig/Nipbl.unique.dupmark.sorted.bw ../008__ChIPseq_Nipbl/output/bigwig/input.unique.dupmark.sorted.bw \
+    --skipZeros \
+    --missingDataAsZero \
+    --blackListFileName ../../Master/meta/hg38-blacklist.v2.bed \
+    -o output/deeptools/matrix_2kb_H3K4me3_QSER1peaksQSER1YAP1genes_Ren.gz \
+    -p 6
+
+
+
+plotHeatmap -m output/deeptools/matrix_2kb_H3K4me3_QSER1peaksQSER1YAP1genes_Ren.gz \
+    -out output/deeptools/matrix_2kb_H3K4me3_QSER1peaksQSER1YAP1genes_Ren_heatmap.pdf \
+    --samplesLabel "QSER1" "YAP1" "H3K4me1" "H3K27ac" "H3K36me3" "H3K27me3" "EZH2" "NIPBL" "input" \
+    --colorMap bwr \
+    --whatToShow 'heatmap and colorbar' \
+    --heatmapHeight 10 \
+    --heatmapWidth 4
+
+
+# interactive
+plotHeatmap -m output/deeptools/matrix_2kb_H3K4me3_QSER1peaksQSER1YAP1genes_Ren.gz \
+    -out output/deeptools/matrix_2kb_H3K4me3_QSER1peaksQSER1YAP1genes_Ren_heatmap1.pdf \
+    --samplesLabel "QSER1" "YAP1" "H3K4me1" "H3K27ac" "H3K36me3" "H3K27me3" "EZH2" "NIPBL" "input" \
+    --colorList 'black, yellow' \
+    --whatToShow 'heatmap and colorbar' \
+    --heatmapHeight 10 \
+    --heatmapWidth 3
+
+
+plotHeatmap -m output/deeptools/matrix_2kb_H3K4me3_QSER1peaksQSER1YAP1genes_Ren.gz \
+    -out output/deeptools/matrix_2kb_H3K4me3_QSER1peaksQSER1YAP1genes_Ren_heatmap2.pdf \
+    --samplesLabel "QSER1" "YAP1" "H3K4me1" "H3K27ac" "H3K36me3" "H3K27me3" "EZH2" "NIPBL" "input" \
+    --colorList 'black, yellow' \
+    --whatToShow 'heatmap and colorbar' \
+    --heatmapHeight 10 \
+    --heatmapWidth 3 \
+    --zMax 20 5 15 15 15 15 15 15 15 15 
+    
+    
+    
+    
+
