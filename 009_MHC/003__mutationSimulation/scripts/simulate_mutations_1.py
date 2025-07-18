abe49cef8597e332902c6ef7ed8faa34503c0ed6
Thomas Roule <110408206+RouleThomas@users.noreply.github.com>
2025-07-18 17:13:29 -0400

    uppp

--- 009_MHC/003__mutationSimulation/scripts/simulate_mutations_1.py
+++ 009_MHC/003__mutationSimulation/scripts/simulate_mutations_1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# simulate_mutations_1.py
 
 import argparse, json, pickle
 from pathlib import Path
@@ -67,33 +66,73 @@ class ExomeSampler:
             np.array(revcomp_flags, dtype=bool)
         )
 
+
+
+
+
     def annotate(self, tbl: pa.Table, ctx_ids: np.ndarray, revcomp_flags: np.ndarray) -> pd.DataFrame:
         df = tbl.to_pandas()
         df["context_id"] = ctx_ids
         df["revcomp"] = revcomp_flags
 
-        alt_bases = [CONTEXTS_96[c][5] for c in ctx_ids]  # Extract ALT from e.g., A[C>T]G
+        mutated_codons = []
+        aa_alt = []
+        consequences = []
         ref_bases = []
-        alt_bases_corrected = []
+        alt_bases = []
 
-        for ref_codon, idx, alt, flag in zip(df.ref_codon, df.codon_index, alt_bases, df.revcomp):
-            cod = list(ref_codon)
-            if flag:
-                cod_rc = list(reverse_complement("".join(cod)))
-                ref_base = cod_rc[2 - idx]
-                alt_base = reverse_complement(alt)
+        for i in range(len(df)):
+            ref_codon = df.at[i, "ref_codon"]
+            codon_index = df.at[i, "codon_index"]
+            revcomp = revcomp_flags[i]
+            ctx_id = ctx_ids[i]
+            context = CONTEXTS_96[ctx_id]
+
+            codon = list(ref_codon)
+
+            if revcomp:
+                ref_base = reverse_complement(context[2])
+                alt_base = reverse_complement(context[6])
+                codon_rc = list(reverse_complement("".join(codon)))
+                codon_rc[2 - codon_index] = alt_base
+                mutated_codon = reverse_complement("".join(codon_rc))
             else:
-                ref_base = cod[idx]
-                alt_base = alt
+                ref_base = context[2]
+                alt_base = context[3]
+                codon[codon_index] = alt_base
+                mutated_codon = "".join(codon)
 
             ref_bases.append(ref_base)
-            alt_bases_corrected.append(alt_base)
+            alt_bases.append(alt_base)
+            mutated_codons.append(mutated_codon)
 
+            if mutated_codon in STOP_CODONS:
+                aa_alt.append("*")
+                consequences.append("stop")
+            else:
+                aa_new = CODON_TABLE.get(mutated_codon)
+                aa_ref = CODON_TABLE.get(ref_codon)
+                if aa_new is None:
+                    aa_alt.append(None)
+                    consequences.append("unknown")
+                elif aa_new == aa_ref:
+                    aa_alt.append(aa_new)
+                    consequences.append("synonymous")
+                else:
+                    aa_alt.append(aa_new)
+                    consequences.append("missense")
+
+        df["mut_codon"] = mutated_codons
+        df["alt_aa"] = aa_alt
+        df["consequence"] = consequences
         df["ref_base"] = ref_bases
-        df["alt_base"] = alt_bases_corrected
+        df["alt_base"] = alt_bases
 
-        # Optionally keep codon/consequence logic
         return df
+
+
+
+
 
 def main():
     p = argparse.ArgumentParser(description="Simulate mutations from an SBS signature")
