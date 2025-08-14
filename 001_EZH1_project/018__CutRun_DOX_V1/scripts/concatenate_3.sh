0d285a901e982d372590a74264b35c72c9233d4a
RouleThomas <roule@sas.upenn.edu>
2025-08-14 15:35:54 -0400

    up

--- 001_EZH1_project/018__CutRun_DOX_V1/scripts/concatenate_3.sh
+++ 001_EZH1_project/018__CutRun_DOX_V1/scripts/concatenate_3.sh
@@ -7,6 +7,9 @@
 
 
 
+cat input_raw_Novogene/R1_ESC_OEKO_EZH1_CKDL250021971-1A_22W5G2LT4_L2_1.fq.gz \
+input_raw_Novogene/R1_ESC_OEKO_EZH1_CKDL250021971-1A_22W5TMLT4_L3_1.fq.gz > \
+input/ESC_OEKO_EZH1_R1_1.fq.gz
 
 
 
